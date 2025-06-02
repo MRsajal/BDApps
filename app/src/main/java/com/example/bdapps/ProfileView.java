e525bec7fa888c9568f4e9ada5cbfc154b9c1ab1
shadiq <srs.shadiq@gmal.com>
2025-06-02 21:10:31 +0600

    profile created

---
+++ app/src/main/java/com/example/bdapps/ProfileView.java
@@ -0,0 +1,34 @@
+package com.example.bdapps;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+public class ProfileView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_view);
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        ViewPager2 viewPager = findViewById(R.id.view_pager);
+
+        ProfileViewTabsAdapter adapter = new ProfileViewTabsAdapter(this);
+        viewPager.setAdapter(adapter);
+
+        new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> {
+            if (position == 0) tab.setText("About");
+            else tab.setText("Posts");
+        }).attach();
+
+
+    }
+}
