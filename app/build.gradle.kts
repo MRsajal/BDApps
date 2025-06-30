3173a54c556d0c04d64c95bb1b3cecd91908b653
Sajal <mostafizurrahamnsajal8@gmail.com>
2025-06-30 06:18:57 +0600

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation("androidx.navigation:navigation-ui:2.7.7")
     testImplementation("junit:junit:4.13.2")
     implementation("com.github.bumptech.glide:glide:4.16.0")
+
     annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
